033f063aa2f66093b6d6934ed0eee74ce9e63c8c
unknown <steph.shipley@yahoo.com>
2019-05-15 10:32:35 -0400

    create initial tests for game

---
+++ src/main/java/GuessingGame.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class GuessingGame {
+
+	public String playgame(int i) {
+		// TODO Auto-generated method stub
+		return ("You Win");
+	}
+
+}
+public String playGame(int yourNumber)
+
+	@Test
+	public void shouldSayYouWinWhenNumbersMatch() {
+  
+  GuessingGame undertest = new GuessingGame();
+  String response = undertest. playgame(7);
+  equals("You Win")
+}
---
+++ src/test/java/GuessingGameTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class GuessingGameTest {
+
+	      public String playGame(int yourNumber)
+	      
+	      	@Test
+	      	public void shouldSayYouWinWhenNumbersMatch() {
+	    	  
+	    	  GuessingGame undertest = new GuessingGame();
+	    	  String response = undertest. playgame(7);
+	    	  equals("You Win")
+	      }
+}
